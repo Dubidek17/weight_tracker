3a366dcf0597351039e45ae3900dbf72fffae990
Dubidek <dubidek17@gmail.com>
2025-12-15 12:21:28 +0100

    added simple history layout and started to create data download mechanism

--- app/src/main/java/com/example/weighttracker/MainActivity.java
+++ app/src/main/java/com/example/weighttracker/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
     private Button mainScreenButton;
     private Button statsScreenButton;
     private Button historyScreenButton;
+    private EditText editTextWeight;
+    private EditText editTextDate;
+    private Button submitButton;
+    private Double weight;
+    private String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,18 @@ public class MainActivity extends AppCompatActivity {
         mainScreenButton = findViewById(R.id.mainScreenButton);
         statsScreenButton = findViewById(R.id.statsScreenButton);
         historyScreenButton = findViewById(R.id.historyScreenButton);
+
+        editTextWeight = findViewById(R.id.editTextWeight);
+        editTextDate = findViewById(R.id.editTextDate);
+        submitButton = findViewById(R.id.submitButton);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                weight = Double.parseDouble(editTextWeight.getText().toString());
+                date = editTextWeight.getText().toString();
+            }
+        });
 
         statsScreenButton.setOnClickListener(new View.OnClickListener() {
             @Override
